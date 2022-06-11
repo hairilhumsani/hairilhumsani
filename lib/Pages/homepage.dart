57aed60819a0347ce1ff6adc7e688c31f8b884e9
hairilhumsani <hairilhumsani@gmail.com>
2022-06-11 18:42:59 +0800

    added template

---
+++ lib/Pages/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hairilhumsani/Controllers/responsive/desktop_body.dart';
+import 'package:hairilhumsani/Controllers/responsive/mobile_body.dart';
+import 'package:hairilhumsani/Controllers/responsive/responsive_layout.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ResponsiveLayout(
+        mobileBody: const MyMobileBody(),
+        desktopBody: const MyDesktopBody(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-// ignore: unused_import
-import 'package:carousel_slider/carousel_controller.dart';
+import 'package:hairilhumsani/Pages/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,41 +7,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hairil Humsani',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      darkTheme: ThemeData.dark(),
-      home: const MyHomePage(title: 'Welcome To My Flutter Page'),
+      home: const HomePage(),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  final TabController _tabController;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-      return <Widget>[
-        SliverAppBar(
-          title: Text("Weight Tracker"),
-          pinned: true,
-          floating: true,
-          forceElevated: innerBoxIsScrolled,
-          bottom: TabBar(tabs: <Tabs>[Tab(text: 'HELLO'), Tab(text: 'BYE')]
-          controller: _tabController,),
-        )
-      ];
-    }));
   }
 }
