a64716e49dff7d27615c1b9c013964c89ebe8a47
hairilhumsani <hairilhumsani@gmail.com>
2022-06-15 14:13:29 +0800

    heh

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hairilhumsani/Controllers/navbar/navbar.dart';
-import 'package:hairilhumsani/Controllers/responsive/desktop_body.dart';
-import 'package:hairilhumsani/Controllers/responsive/mobile_body.dart';
-import 'package:hairilhumsani/Controllers/responsive/responsive_layout.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,13 +13,20 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const NavBar(),
-      backgroundColor: Colors.deepPurple[200],
-      appBar: AppBar(title: const Text('PORTFOLIO')),
-      body: const ResponsiveLayout(
-        mobileBody: MyMobileBody(),
-        desktopBody: MyDesktopBody(),
-      ),
+        drawer: const NavBar(),
+        backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(title: const Text('PORTFOLIO')),
+        body: const HomeBuild());
+  }
+}
+
+class HomeBuild extends StatelessWidget {
+  const HomeBuild({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple[300],
     );
   }
 }
